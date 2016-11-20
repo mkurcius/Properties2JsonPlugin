dc4a0876a0264af174dfd95a6068049764918a57
mkurcius <mkurcsiu@gmail.com>
2016-11-20 18:48:16 +0100

    [fix] webpack --watch

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const globby = require('globby');
 const properties = require('properties-parser');
+const path = require("path");
 const extend = require('util')._extend;
 
 function Properties2JsonPlugin(options) {
@@ -7,44 +8,43 @@ function Properties2JsonPlugin(options) {
 }
 
 Properties2JsonPlugin.prototype.apply = function (compiler) {
-  var opt = Array.isArray(this.opt) ? this.opt : [this.opt];
+  let opt = Array.isArray(this.opt) ? this.opt : [this.opt];
 
   compiler.plugin('emit', function (compilation, callback) {
-    var promises = [];
+    let promises = [];
     opt.forEach(obj => promises.push(worker(obj, compilation)));
 
     Promise.all(promises)
-      .then(() => { callback(); })
+      .then(() => {
+        callback();
+      })
       .catch(callback);
   });
 
 };
 
 function worker(obj, compilation) {
-  return globby(obj.files).then(paths => {
-    var json = propertiesToJson(paths);
-    addAsset(compilation, json, obj.output);
+  return globby(obj.files).then(filea => {
+    let json = propertiesToJson(filea);
+    filea.forEach(file => {
+      file = path.resolve(compilation.compiler.context, file);
+      compilation.fileDependencies.push(file);
+    });
+
+    compilation.assets[obj.output] = {
+      source: () => json,
+      size: () => json.length
+    };
   });
 }
 
 function propertiesToJson(paths) {
-  var json = {};
+  let json = {};
   paths.forEach(path => {
     extend(json, properties.read(path));
   });
 
   return JSON.stringify(json);
-}
-
-function addAsset(compilation, json, output) {
-  compilation.assets[output] = {
-    source: function () {
-      return json;
-    },
-    size: function () {
-      return json.length;
-    }
-  };
 }
 
 module.exports = Properties2JsonPlugin;
